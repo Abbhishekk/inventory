532bf38eb687be8ac6e6bc03e7a25454548c0dc9
Abbhishekk <abhishek.bante.cse@ghrce.raisoni.net>
2025-09-14 00:09:54 +0530

    Fix api-gateway

--- product-service/src/main/java/com/inventoryManagement/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/inventoryManagement/product_service/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
 
