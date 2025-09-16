f5f262eb418492dbf1b64145b2213d8c426f6a7e
Bante <abhishek-eknath.bante@capgemini.com>
2025-09-16 10:26:10 +0530

    Added email to notify

--- api/auth-service/src/main/java/com/tm/auth/AuthConfig.java
+++
@@ -1,9 +0,0 @@
-package com.tm.auth;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AuthConfig {
-    // Configuration class for auth service
-    // RestTemplate removed in favor of Feign clients
-}
